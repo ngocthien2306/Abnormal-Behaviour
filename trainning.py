8c2691e2d3b2ba09947ea74c63da20978a9b751a
DatNgo <ngominhdat110115@gmail.com>
2022-05-18 17:16:37 +0700

    fine tune

--- trainning.py
+++ trainning.py
@@ -8,8 +8,8 @@ Created on Wed May 18 09:37:50 2022
 import pandas as pd
 import numpy as np
 from statistics import mean
+import joblib
 
-import joblib # new lib
 def store_model(model, model_name = ""):
     # NOTE: sklearn.joblib faster than pickle of Python
     # INFO: can store only ONE object in a file
@@ -22,7 +22,6 @@ def load_model(model_name):
     model = joblib.load('models/' + model_name + '_model.pkl')
     #print(model)
     return model
-
 
 raw_data = pd.read_csv('trainning_data.csv')
 
@@ -49,7 +48,91 @@ train_set = train_set.drop(columns = "label")
 test_set_labels = test_set["label"].copy()
 test_set = test_set.drop(columns = "label") 
 
-# In[3]: Trainning data
+# In[3]: Fine turnning
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import KFold   
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+new_tuning = False
+
+grid_search = None
+C = 0
+gamma = ''
+kernel = ''
+if new_tuning:
+    cv = KFold(n_splits=5,shuffle=True,random_state=37)
+    model = SVC()
+    param_grid = [
+        # Try 3
+        {
+         'kernel':['rbf'],
+         'C': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], 
+         'gamma': ['scale']
+        },
+
+    ];
+    grid_search = GridSearchCV(model, param_grid, cv=cv)
+    grid_search.fit(train_set, train_set_labels)
+    joblib.dump(grid_search,'saved_objects/SVC_gridsearch.pkl')
+    (c, gamma, kernel) = grid_search.best_params_
+    print(grid_search.best_params_)
+    print(grid_search.best_score_)  
+    print(grid_search.best_estimator_.C)
+    C       = grid_search.best_estimator_.C
+    gamma   = grid_search.best_estimator_.gamma
+    kernel  = grid_search.best_estimator_.kernel
+else:
+    grid_search = joblib.load('saved_objects/SVC_gridsearch.pkl')
+    C       = grid_search.best_estimator_.C
+    gamma   = grid_search.best_estimator_.gamma
+    kernel  = grid_search.best_estimator_.kernel
+
+# Try 1
+"""
+{
+ 'kernel':['rbf'],
+ 'C': [0.1, 10, 50, 100], 
+ 'gamma': ['scale', 'auto']
+},
+"""
+# Try 2
+"""
+{
+ 'kernel':['poly'],
+ 'C': [0.1, 10, 50], 
+ 'degree': [3, 10, 20], 
+ 'gamma': ['scale', 'auto'],
+ 'coef0': [1, 5, 10], 
+},
+"""
+# Try 3
+"""
+{
+ 'kernel':['rbf'],
+ 'C': [5, 8, 10, 20, 30], 
+ 'gamma': ['scale']
+},
+"""
+# Try 4
+"""
+{
+ 'kernel':['rbf'],
+ 'C': [1, 2, 3, 4, 5], 
+ 'gamma': ['scale']
+},
+"""
+# Try 4
+"""
+{
+ 'kernel':['rbf'],
+ 'C': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], 
+ 'gamma': ['scale']
+},
+"""
+
+# In[4]: Trainning data
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
@@ -57,7 +140,7 @@ from sklearn.svm import SVC
 new_training = True
 clf = None
 if new_training:
-    clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+    clf = make_pipeline(StandardScaler(), SVC(C=C,kernel=kernel, gamma=gamma))
     clf.fit(train_set, train_set_labels)
     store_model(clf, model_name = "SVC")      
 else:
@@ -65,28 +148,7 @@ else:
 
 print(test_set_labels)
 print(clf.predict(test_set))
-
-# In[4]: Validation
-from sklearn.model_selection import cross_val_score
-print('\n____________ K-fold cross validation ____________')
-
-run_evaluation = True
-if run_evaluation:
-    from sklearn.model_selection import KFold
-    cv = KFold(n_splits=5,shuffle=True,random_state=37) # cv data generator: just a try to persist data splits (hopefully)
-
-    model_name = "SVC" 
-    nmse_scores = cross_val_score(clf, train_set, train_set_labels, cv=3, scoring='neg_mean_squared_error')
-    rmse_scores = np.sqrt(nmse_scores)
-    joblib.dump(rmse_scores,'saved_objects/' + model_name + '_rmse.pkl')    
-    print("SVC rmse: ", rmse_scores.round(decimals=1))
-    print("Avg. rmse: ", mean(rmse_scores.round(decimals=1)),'\n')
-else:
-    # Load rmse
-    model_name = "SVC" 
-    rmse_scores = joblib.load('saved_objects/' + model_name + '_rmse.pkl')
-    print("\SVC rmse: ", rmse_scores.round(decimals=1))
-    print("Avg. rmse: ", mean(rmse_scores.round(decimals=1)),'\n')
-
+print('Score:')
+print(clf.score(test_set, test_set_labels))
 
 
